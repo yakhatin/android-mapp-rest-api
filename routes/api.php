42e12856c2790889f05453619dcf5fb4e3d882bf
Alexander Yakhatin <sasha.yahatin@yandex.ru>
2020-11-03 17:30:51 +0300

    Added auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -18,23 +18,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('/catalogs')->group(function () {
-    Route::get('/', 'CatalogController@get');
-    Route::post('/', 'CatalogController@create');
-    Route::delete('/{id}', 'CatalogController@delete');
-    Route::put('/{id}', 'CatalogController@update');
-});
+/**
+ * Роуты требующие токена-авторизации
+ */
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('/catalogs')->group(function () {
+        Route::get('/', 'CatalogController@get');
+        Route::post('/', 'CatalogController@create');
+        Route::delete('/{id}', 'CatalogController@delete');
+        Route::put('/{id}', 'CatalogController@update');
+    });
 
-Route::prefix('/articles')->group(function () {
-    Route::get('/', 'ArticleController@get');
-    Route::post('/', 'ArticleController@create');
-    Route::delete('/{id}', 'ArticleController@delete');
-    Route::put('/{id}', 'ArticleController@update');
-});
+    Route::prefix('/articles')->group(function () {
+        Route::get('/', 'ArticleController@get');
+        Route::post('/', 'ArticleController@create');
+        Route::delete('/{id}', 'ArticleController@delete');
+        Route::put('/{id}', 'ArticleController@update');
+    });
 
-Route::prefix('/commentaries')->group(function () {
-    Route::get('/', 'CommentaryController@get');
-    Route::post('/', 'CommentaryController@create');
-    Route::delete('/{id}', 'CommentaryController@delete');
-    Route::put('/{id}', 'CommentaryController@update');
+    Route::prefix('/commentaries')->group(function () {
+        Route::get('/', 'CommentaryController@get');
+        Route::post('/', 'CommentaryController@create');
+        Route::delete('/{id}', 'CommentaryController@delete');
+        Route::put('/{id}', 'CommentaryController@update');
+    });
 });
